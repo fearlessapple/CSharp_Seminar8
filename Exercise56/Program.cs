fa34d85b3482a9026c9e68403bc17002f6b17d1a
Stas <smaster950@gmail.com>
2022-11-12 20:04:41 +0300

    Complete Exercise56

--- Exercise56/Program.cs
+++ Exercise56/Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine() ?? "");
+
+int[,] array = GetArray(rows, columns);
+PrintArray(array);
+Console.WriteLine();
+FindRowinArray(array);
+
+int[,] GetArray(int m, int n)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(0, 10);
+        }
+    }
+    return result;
+}
+
+void FindRowinArray(int[,] array)
+{
+    int row = 0;
+    int nextSum = Int32.MaxValue;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum = sum + array[i, j];
+        }
+        if (sum < nextSum)
+        {
+            nextSum = sum;
+            row++;
+        }
+    }
+    Console.WriteLine($"Строка с наименьшей суммой значений элементов строки = {row}");
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
